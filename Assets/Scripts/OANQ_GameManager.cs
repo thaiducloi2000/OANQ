8751749d4dbb28e9750d35a654279d452e61bdac
Little Kid <74088371+thaiducloi2000@users.noreply.github.com>
2022-11-21 21:02:43 +0700

    Add End Game

--- Assets/Scripts/OANQ_GameManager.cs
+++ Assets/Scripts/OANQ_GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class OANQ_GameManager : MonoBehaviour
 {
     public static OANQ_GameManager instance;
-    public enum GameStatus { Pause,Playing}
+    public enum GameStatus { Pause,Playing,End}
     public GameStatus status;
     private void Awake()
     {
@@ -16,8 +16,22 @@ public class OANQ_GameManager : MonoBehaviour
         this.status = GameStatus.Playing;
     }
 
+
+    private void Update()
+    {
+        if (isEndGame())
+        {
+            this.status = GameStatus.End;
+            EndGame();
+        }
+    }
     public void EndGame()
     {
         UIManager.instance.QuitBtn();
+    }
+
+    private bool isEndGame()
+    {
+        return Chessboard.Instance.listNode[0].GetComponentInChildren<Node>()._CurrentNumchess == 0 && Chessboard.Instance.listNode[6].GetComponentInChildren<Node>()._CurrentNumchess == 0;
     }
 }
